e9c3f91a702fc7ae90a0ea79008ae311413081e6
Stephen Hack <stephen.hack@gmail.com>
2018-11-18 16:49:19 -0500

    updated table with mock data

---
+++ src/app/body/report.interface.ts
@@ -0,0 +1,16 @@
+export interface Report {
+  blsEmploymentMay2018: number;
+  blsEmploymentPerThousandMay2018: number;
+  blsMeanWageMay2018: number;
+  day: number;
+  glassdoorJobs: number;
+  glassdoorSalary: number;
+  glassdoorSalarySamples: number;
+  indeedJobs: number;
+  indeedSalary: number;
+  indeedSalarySamples: number;
+  title: string;
+  metro: string;
+  month: number;
+  year: number;
+}
--- src/app/body/table/table.component.ts
+++ src/app/body/table/table.component.ts
@@ -1,24 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+import { CareerService } from '../career-service.service';
+import { Report } from './../report.interface';
 
 @Component({
   selector: 'app-table',
@@ -27,10 +9,26 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class TableComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  test: Report[] = [
+    { blsEmploymentMay2018: 1, blsEmploymentPerThousandMay2018: 1, blsMeanWageMay2018: 1,
+      day: 1, glassdoorJobs: 1, glassdoorSalary: 1, glassdoorSalarySamples: 1,
+      indeedJobs: 1, indeedSalary: 1, indeedSalarySamples: 1, title: 'Dev', metro: 'NYC',
+      month: 12, year: 2011},
+    { blsEmploymentMay2018: 1, blsEmploymentPerThousandMay2018: 1, blsMeanWageMay2018: 1,
+      day: 1, glassdoorJobs: 1, glassdoorSalary: 1, glassdoorSalarySamples: 1,
+      indeedJobs: 1, indeedSalary: 1, indeedSalarySamples: 1, title: 'Dev', metro: 'NYC',
+      month: 12, year: 2011},
+    { blsEmploymentMay2018: 1, blsEmploymentPerThousandMay2018: 1, blsMeanWageMay2018: 1,
+      day: 1, glassdoorJobs: 1, glassdoorSalary: 1, glassdoorSalarySamples: 1,
+      indeedJobs: 1, indeedSalary: 1, indeedSalarySamples: 1, title: 'Dev', metro: 'NYC',
+      month: 12, year: 2011},
+  ];
 
-  constructor() { }
+  displayedColumns: string[] = ['blsEmploymentMay2018', 'blsEmploymentPerThousandMay2018',
+  'blsMeanWageMay2018', 'day', 'glassdoorJobs', 'glassdoorSalary', 'glassdoorSalarySamples',
+  'indeedJobs', 'indeedSalary', 'indeedSalarySamples', 'title', 'metro', 'month', 'year'];
+
+  constructor(private careerService: CareerService) { }
 
   ngOnInit() {
   }
